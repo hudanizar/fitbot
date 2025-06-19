76bc530c5735182b4f43fce1975fc9626c9c5778
hudanizar <136156300+hudanizar@users.noreply.github.com>
2025-06-19 08:50:47 +0800

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 import scipy.cluster.hierarchy as sch
 import plotly.express as px
